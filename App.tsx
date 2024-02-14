32381fe379a19d949029f4fb0578da19626b332e
ichigo0126 <luckyshoma@gmail.com>
2024-02-15 01:38:29 +0900

    Profile画面作成

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { HomeScreen } from "./screens/HomeScreen";
 import { CalendarScreen } from "./screens/CalendarScreen";
+import { MeasurementScreen } from "./screens/MeasurementScreen";
+import { ProfileScreen } from "./screens/ProfileScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MeasurementScreen } from "./screens/MeasurementScreen";
 import { FontAwesome } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
@@ -31,6 +31,8 @@ const screenOptions = ({ route }: ScreenOptionsProps) => ({
       return <FontAwesome name="list" size={size} color={color} />;
     } else if (route.name === "CalendarTab") {
       return <FontAwesome name="calendar" size={size} color={color} />;
+    } else if (route.name === "ProfileTab") {
+      return <FontAwesome name="user-o" size={size} color={color} />;
     }
     return null;
   },
@@ -62,8 +64,19 @@ const CalendarStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name="Home"
+        name="Calendar"
         component={CalendarScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+const ProfileStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
@@ -76,20 +89,25 @@ export default function App() {
       <Tab.Navigator screenOptions={screenOptions}>
         {/* <Tab.Navigator> */}
         <Tab.Screen
-          name="MeasurementTab"
-          component={MeasurementStack}
-          options={{ title: "CalorieCalculation" }}
-        />
-        <Tab.Screen
           name="HomeTab"
           component={HomeStack}
           options={{ title: "Home" }}
+        />
+        <Tab.Screen
+          name="MeasurementTab"
+          component={MeasurementStack}
+          options={{ title: "CalorieCalculation" }}
         />
 
         <Tab.Screen
           name="CalendarTab"
           component={CalendarStack}
           options={{ title: "Calendar" }}
+        />
+        <Tab.Screen
+          name="ProfileTab"
+          component={ProfileStack}
+          options={{ title: "Profile" }}
         />
       </Tab.Navigator>
     </NavigationContainer>
---
+++ screens/ProfileScreen.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import { Text } from "react-native-paper";
+import { TextInput, Button } from "react-native-paper";
+
+export const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <View>
+          <Text variant="headlineSmall">体重</Text>
+          <TextInput
+            style={styles.input}
+            mode="outlined"
+            label="体重を記入してください"
+          />
+        </View>
+        <View>
+          <Text variant="headlineSmall">身長</Text>
+          <TextInput
+            style={styles.input}
+            mode="outlined"
+            label="身長を記入してください"
+          />
+        </View>
+        <View>
+          <Button mode="contained" onPress={() => console.log("Pressed")}>
+            保存
+          </Button>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "rgb(238, 238, 238)",
+    borderRadius: 20,
+    borderWidth: 1,
+    width: 250,
+  },
+  buttonText: {
+    textAlign: "center",
+    padding: 10,
+  },
+  input: {
+    width: 200,
+    marginBottom: 10,
+  },
+});
