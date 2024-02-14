86d064ea6b7861adbe6210584bbd7f5c4ea3cb40
ichigo0126 <luckyshoma@gmail.com>
2024-02-14 22:40:31 +0900

    歩数計実装

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
@@ -5,10 +6,33 @@ import {
   SafeAreaView,
   TouchableOpacity,
 } from "react-native";
-import React, { useState } from "react";
+import { Pedometer } from "expo-sensors";
 
 export const HomeScreen = () => {
   const [isStart, setIsStart] = useState(true);
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState("checking");
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+  const [subscription, setSubscription] = useState(Object);
+
+  const subscribe = async () => {
+    const isAvailable = await Pedometer.isAvailableAsync();
+    setIsPedometerAvailable(String(isAvailable));
+
+    if (isAvailable) {
+      const end = new Date();
+      const start = new Date();
+      start.setDate(end.getDate() - 1);
+
+      return Pedometer.watchStepCount((result) => {
+        setCurrentStepCount(result.steps);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const subscription: any = subscribe();
+    return () => subscription && subscription.remove();
+  }, []);
 
   const toggleButton = () => {
     setIsStart(!isStart);
@@ -21,6 +45,10 @@ export const HomeScreen = () => {
           <View style={styles.buttonContainer}>
             <Text style={styles.buttonText}>{isStart ? "開始" : "終了"}</Text>
           </View>
+          {!isStart && (
+            <Text>機能が使えるかどうか: {isPedometerAvailable}</Text>
+          )}
+          <View>{!isStart && <Text>歩数: {currentStepCount}</Text>}</View>
         </TouchableOpacity>
       </SafeAreaView>
     </View>
